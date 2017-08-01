2a91fe2bb7fd03c325d0eef6516aceab9ce8c3f3
Boly38 <Boly38@gmail.com>
2017-08-01 22:19:59 +0200

    clean

--- maPosition.php
+++ maPosition.php
@@ -1,18 +1,7 @@
 <?php
-
 include "MaPosition.inc";
-
 include "MaCache.inc";
 
-/*
-$query = @unserialize (file_get_contents('http://ip-api.com/php/'));
-if ($query && $query['status'] == 'success') {
-echo 'Hey user from ' . $query['country'] . ', ' . $query['city'] . '!';
-}
-foreach ($query as $data) {
-    echo $data . "<br>";
-}
-*/
 	$mp = new MaPosition();
 	$mp->locateMe();
 	if (!$mp->isLocated()) {
